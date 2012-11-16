9f3c4552d3aaf97cd7c166b116bf075e38271015
Michał Buś <michal.bus@mendeley.com>
2012-11-16 12:20:23 +0000

    Adding some more stuff

--- run.rb
+++ run.rb
@@ -2,9 +2,11 @@ require 'rubygems'
 require 'watir-webdriver'
 require 'net/http'
 require 'json'
+require 'yaml'
 
 
-consumer_key = '4654e0c31ad8fa1dbaed683f1e47027404d357753'
+key = YAML.load_file('keys.yml')["consumer_key"]
+
 print "Type in your search term: "
 search_term = gets.chomp
 print "What language do you like? Describe it using two (or more, if you want Chinese) letters. (Examples that work nicely: es, pl, it, la, de, ar, fr, hu, sv, ja, ko, zh-TW: "
@@ -12,7 +14,7 @@ language = gets.chomp
 
 
 
-search_result = Net::HTTP.get('api.mendeley.com', URI.escape('/oapi/documents/search/' + search_term + '/?consumer_key=' + consumer_key + '&items=10'))
+search_result = Net::HTTP.get('api.mendeley.com', URI.escape('/oapi/documents/search/' + search_term + '/?consumer_key=' + key + '&items=10'))
 search_result_json = JSON.parse(search_result)
 search_documents = search_result_json['documents']
 
@@ -26,13 +28,13 @@ doc_title = nil
 doc_abstract = nil
 
 until (doc_title != nil && doc_abstract != nil) do
-  doc_details = JSON.parse(Net::HTTP.get('api.mendeley.com', URI.escape('/oapi/documents/details/' + doc_ids[n] + '/?consumer_key=' + consumer_key)))
+  doc_details = JSON.parse(Net::HTTP.get('api.mendeley.com', URI.escape('/oapi/documents/details/' + doc_ids[n] + '/?consumer_key=' + key)))
   doc_title = doc_details["title"]
   doc_abstract = doc_details["abstract"]
   n = n + 1
 end
 
-translate_request_url = 'http://translate.google.com/#auto/' + language + '/' + URI.escape("Title: " + doc_title + " Abstract: " + doc_abstract)
+translate_request_url = 'http://translate.google.com/#auto/' + language + '/' + URI.escape(doc_title + ". " + doc_abstract)
 
 $rain = Watir::Browser.new(:firefox)
 $rain.goto('http://www.rainymood.com/')
@@ -41,7 +43,7 @@ $b = Watir::Browser.new(:firefox)
 
 if translate_request_url.length > 2000
   $b.goto('http://translate.google.com/#auto/' + language + '/')
-  $b.textarea(:id => 'source').set("Title: " + doc_title + " Abstract: " + doc_abstract)
+  $b.textarea(:id => 'source').set(doc_title + ". " + doc_abstract)
 else
   $b.goto(translate_request_url)
 end
